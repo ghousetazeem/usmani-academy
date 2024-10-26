7aa7c514c265ccb5eb8a3793b3e6778663828f83
ghousetazeem <ghousetazeem75@gmail.com>
2024-10-25 22:57:58 -0500

    Updated the code

--- src/components/art/ArtCard.jsx
+++ src/components/art/ArtCard.jsx
@@ -1,49 +1,35 @@
-import React, { useState } from "react";
-import { blog } from "../../dummydata";
-import "./art.css"; // Assuming you'll add custom CSS here
+import React from 'react';
+import './art.css';
+import { Link } from 'react-router-dom';
+import { blog } from '../../dummydata';
 
 const ArtCard = () => {
-    const [selectedImage, setSelectedImage] = useState(null);
-
-    const handleImageClick = (imageSrc) => {
-        setSelectedImage(imageSrc);
-    };
-
-    const closeModal = () => {
-        setSelectedImage(null);
-    };
-
     return (
         <>
-            <div className={selectedImage ? "blur-background" : ""}>
-                {blog.map((val, index) => (
-                    <div className="blog-card items shadow" key={index}>
-                        <div className="img" onClick={() => handleImageClick(val.cover)}>
-                            <img src={val.cover} alt={val.title} />
-                        </div>
-                        <div className="text">
-                            <div className="admin flexSB">
-                                <span>
-                                    <i className="fa fa-area-chart"></i>
-                                    <label htmlFor="">{val.type}</label>
+            <section className="coursesCard" style={{ marginTop: "6rem", marginBottom: "8rem", paddingTop: "0", paddingRight: "6rem" }}>
+                <div className="mycontainer grid2">
+                    {blog.map((val) => (
+                        <div className="items " key={val.id}>
+                            <div className="content flex">
+                                <img src={val.cover} />
+                            </div>
+                            <div className='admin flexSB'>
+                                <span style={{ display: "flex", flexDirection: "row", margin: "16px", color: "#273e06" }}>
+                                    <i className='fa fa-image'></i>
+                                    <label htmlFor='' style={{ marginLeft: "0.6rem" }}>{val.type}</label>
                                 </span>
                             </div>
-                            <h1>{val.title}</h1>
-                            <button className="blog-btn">Buy</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
 
-            {/* Modal for full-screen image */}
-            {selectedImage && (
-                <div className="modal" onClick={closeModal}>
-                    <span className="close-modal">&times;</span>
-                    <div className="modal-content-wrapper">
-                        <img className="modal-content" src={selectedImage} alt="Full Screen" />
-                    </div>
+                            <div style={{ margin: "16px", marginTop: "0", color: "#273e06", fontSize: "1.4rem", textAlign: "left", fontWeight: "bold" }}>
+                                <h2 >{val.title}</h2>
+                            </div>
+                            <Link to={`/${val.resourceRoute}`}> {/* Ensure route starts with / */}
+                                <button className="outline-btn">See more...</button>
+                            </Link>
+                        </div>
+                    ))}
                 </div>
-            )}
+            </section>
         </>
     );
 };
